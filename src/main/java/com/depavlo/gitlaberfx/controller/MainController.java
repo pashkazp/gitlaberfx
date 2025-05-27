baae7a9faba3130e585c450e82b62ceabb3f564e
pashkazp <pashka@ukr.net>
2025-05-27 21:32:15 +0300

    Disable branchesTableView during background operations
        Added logic to disable branchesTableView while background tasks are running to prevent unintended selection changes. This ensures a smoother and more predictable user experience.

--- src/main/java/com/depavlo/gitlaberfx/controller/MainController.java
+++ src/main/java/com/depavlo/gitlaberfx/controller/MainController.java
@@ -1067,6 +1067,9 @@ public class MainController {
             // Disable/enable comboboxes
             projectComboBox.setDisable(disable);
             mainBranchComboBox.setDisable(disable);
+
+            // Disable/enable branchesTableView to prevent selection changes during background operations
+            branchesTableView.setDisable(disable);
         });
     }
 
