f3c7cd3d3243e5e0fcb9c62ca8dd0562b779c34d
pashkazp <pashka@ukr.net>
2025-05-28 22:11:41 +0300

    Update copyright to 2025 and add MIT license headers to all source files

--- src/test/java/com/depavlo/gitlaberfx/model/BranchModelTest.java
+++ src/test/java/com/depavlo/gitlaberfx/model/BranchModelTest.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Pavlo Dehtiarov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.depavlo.gitlaberfx.model;
 
 import javafx.beans.property.BooleanProperty;
@@ -11,7 +34,7 @@ class BranchModelTest {
     @Test
     void testConstructor() {
         BranchModel branch = new BranchModel("test-branch", "2024-01-01", true);
-        
+
         assertEquals("test-branch", branch.getName());
         assertEquals("2024-01-01", branch.getLastCommit());
         assertTrue(branch.isMerged());
@@ -21,28 +44,28 @@ class BranchModelTest {
     @Test
     void testProperties() {
         BranchModel branch = new BranchModel("test-branch", "2024-01-01", true);
-        
+
         // Перевірка властивостей
         StringProperty nameProperty = branch.nameProperty();
         StringProperty lastCommitProperty = branch.lastCommitProperty();
         BooleanProperty mergedProperty = branch.mergedProperty();
         BooleanProperty selectedProperty = branch.selectedProperty();
-        
+
         assertNotNull(nameProperty);
         assertNotNull(lastCommitProperty);
         assertNotNull(mergedProperty);
         assertNotNull(selectedProperty);
-        
+
         // Перевірка зміни значень
         branch.setName("new-name");
         assertEquals("new-name", branch.getName());
-        
+
         branch.setLastCommit("2024-01-02");
         assertEquals("2024-01-02", branch.getLastCommit());
-        
+
         branch.setMerged(false);
         assertFalse(branch.isMerged());
-        
+
         branch.setSelected(true);
         assertTrue(branch.isSelected());
     }
